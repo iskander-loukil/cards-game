21457295878705a305091a7a0c550a4ff49c1ed6
skanloukil <skanloukil.5@gmail.com>
2021-11-27 16:37:03 +0100

    added cors configuration for bakc end application

--- src/main/java/com/cards/game/GameApplication.java
+++ src/main/java/com/cards/game/GameApplication.java
@@ -2,6 +2,10 @@ package com.cards.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class GameApplication {
@@ -11,4 +15,14 @@ public class GameApplication {
 		SpringApplication.run(GameApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+
+	}
 }
--- src/main/java/com/cards/game/controllers/GameController.java
+++ src/main/java/com/cards/game/controllers/GameController.java
@@ -3,14 +3,15 @@ package com.cards.game.controllers;
 
 import com.cards.game.models.DTO.GameDTO;
 import com.cards.game.services.GameService;
-import com.cards.game.services.OrderGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class GameController {
     private static Logger logger = LoggerFactory.getLogger(GameController.class);
 
